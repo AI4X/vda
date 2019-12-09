2bbeebcffa88ce6dcf414d8d38aa97fc34831d2c
Grossmann Kevin <GrossmannKe>
2019-12-09 15:54:20 +0100

    new commit

---
+++ 12_hodina/grossmann/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import snake from './snake.png';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={snake} className="Snake-img"/>
+        <p>
+          Obrázek z hada.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
